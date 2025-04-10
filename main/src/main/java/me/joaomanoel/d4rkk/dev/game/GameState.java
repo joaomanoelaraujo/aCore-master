e82482958239154a8f37b022fcb119488c9c36fa
joaomanoelaraujo <johnmanoelaraujo@gmail.com>
2025-04-10 12:56:20 -0300

    Minor fixes in: PT_BR.yml and EN_US.yml

--- main/src/main/java/me/joaomanoel/d4rkk/dev/game/GameState.java
+++ main/src/main/java/me/joaomanoel/d4rkk/dev/game/GameState.java
@@ -10,40 +10,40 @@ public enum GameState {
 
   private static Map<String, String> translations;
 
-  // Método para carregar a tradução do idioma da configuração
+
   public static void loadLanguage(FileConfiguration config) {
-    String language = config.getString("language", "portuguese");  // Valor padrão "portuguese"
+    String language = config.getString("defaultLanguage", "PT_BR");
     translations = getTranslationsForLanguage(language);
   }
 
   private static Map<String, String> getTranslationsForLanguage(String language) {
     Map<String, String> translationMap = new HashMap<>();
     switch (language.toLowerCase()) {
-      case "english":
+      case "EN_US":
         translationMap.put("AGUARDANDO", "Waiting for players...");
         translationMap.put("INICIANDO", "Starting game...");
         translationMap.put("EMJOGO", "Game in progress...");
         translationMap.put("ENCERRADO", "Game over!");
         break;
-      case "spanish":
+      case "ES_ES":
         translationMap.put("AGUARDANDO", "Esperando jugadores...");
         translationMap.put("INICIANDO", "Comenzando el juego...");
         translationMap.put("EMJOGO", "Juego en progreso...");
         translationMap.put("ENCERRADO", "¡Juego terminado!");
         break;
-      case "japanese":
+      case "JA_JP":
         translationMap.put("AGUARDANDO", "プレイヤーを待っています...");
         translationMap.put("INICIANDO", "ゲームを開始します...");
         translationMap.put("EMJOGO", "ゲーム進行中...");
         translationMap.put("ENCERRADO", "ゲーム終了！");
         break;
-      case "chinese":
+      case "ZH_CN":
         translationMap.put("AGUARDANDO", "等待 ...");
         translationMap.put("INICIANDO", "游戏开始...");
         translationMap.put("EMJOGO", "游戏进行中...");
         translationMap.put("ENCERRADO", "游戏结束！");
         break;
-      case "portuguese":
+      case "PT_BR":
       default:
         translationMap.put("AGUARDANDO", "Aguardando jogadores...");
         translationMap.put("INICIANDO", "O jogo está começando...");
